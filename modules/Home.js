645f12cc1603666d313577f7dd5a53db28ed2e78
wworveaory <wworveaory@gmail.com>
2021-07-03 17:01:28 +0800

    cds
    cd

--- modules/Home.js
+++ modules/Home.js
@@ -1,58 +1,45 @@
 const db = require(__dirname + '/../modules/mysql2-connect');
-//是一個模組把很多複雜的functio n 路由再取funstion的代號
-// 用sql向dataBass取資料
-// 他就像是一個function 之後會將資料傳給 routes的 home.js
-// 類別一定要大寫，名子取甚麼都可以
-//crud
-//!!!!!!!!!!  類別的第一個字一定要大寫!!!!!!!!!!!!類別
+
 class Home {
     constructor(data) {
         let defaultData = {
             oListId: '',  //訂單流水編號
-            oQty: '',  //商品數量
             oId: '',  //訂單編號
-            oListName: '', //訂單內容流水號
             iId: '', //商品編號
+            oListName: '', //商品名稱
+            oListPrice:'', //商品單價
+            oQty: '',  //商品數量
             iname: '',  //商品名稱
             iImg: '[]',   //商品圖片
             iDiscr: '', //商品簡介
             iprice: '' //商品價格
         };
         this.data = { ...defaultData, ...data };
-        //這只是宣告!!!!
-        //將class Home裡面的data(初始值)，當使用者填入新資料時會用...data(覆蓋值)去覆蓋 
     }
 
-//除純:新增或修改
-//原本沒有今要使要給它金要使，如果我沒有資料就要新增YYY,不投資料表有主KEY和次要KEY
     async save(){
-        if (!this.data.oListId) {
-             let sql = "INSERT INTO `items` SET ?";
-             let [result] = await db.query(sql, [this.data]);
-             //29.把新增的東西+?號傳給RESULT,[]為了是SQL與法一定要用陣列去包他
-             //inserId是新增的 只要有新增了一份ID他就會丟進去舊的ID 只要有新增就會覆蓋9~18 
-            if (result.insertId) {
-                 this.data.oListId = result.insertId;
-                 //把RESULT的KEY丟到 this.data. oListId
-                 return this.data;
-            } else {
-                return false;//新增失敗情況           
+        if(!this.data.oListId){
+            let sql= "INSERT INTO `orders_list` SET ?";
+            let[result]=await db.query(sql,[this.data]);
+            if(result.insertId){
+                this.data.oListId = result.insertId;
+                return this.data;        
+            } else{
+                return false;
             }
-        //如果 S已經有值就更新
-        }  else {
-             const o = {...this.data};
-             delete o.oListId;
-             let sql = "UPDATE`products`SET ? WHERE `oListId`=?";
-             let [result] = await db.query(sql, [o, this.data.oListId]);
-             //給他一ㄍ新的值  45.有幾個問號丟幾個值給他? []  ?? [,]  ??? [,,]
-             // let [result] = await db.query(sql, [this.data,  this.data.uId]);
-            if (result.changedRows) {
-                  return this.data;
-            } else {
-                return false; //沒有修改
-            } 
-        }         
+        }else{
+            const o = {...this.data};
+            delete o.oListId;
+            let sql = "UPDATE `orders_list` SET ? WHERE `oListId`=?";
+            let [result] = await db.query(sql, [o,this.data.oListId]);
+            if(result.changedRows){
+                return this.data;
+            }else{
+                return false;
+            }
+        }    
     }
+   
 
 }
 
@@ -61,3 +48,4 @@ module.exports = Home;
 //匯出home
 
  //33行 db連線  db有含promiss  .query取資料  sql 我要的data  用 array接值  (跟mysql取資料得到的結果 為陣列)
+
--- routes/home.js
+++ routes/home.js
@@ -1,50 +1,39 @@
+//路由是node網站的框架
 //路由設定 小寫
-const express=require("express");
+const express = require("express");
 
-//node網站的框架  
-const router= express.Router()//他是伊格function
-//用express的框架裡的router，express裡面的路由工具  再給router
-// 引入sql
-// const Home=require(__dirname+"/../modules/Home")
+
+const router = express.Router(); //他是一個function
+//用express的框架裡的router，express裡面的路由工具 
+
+const Home=require(__dirname+"/../modules/Home")
+// 引入modules/home裡的sql方法
 //dirname是本地端資料夾的位置 絕對路徑
 //路由連  sql向dataBass取資料(他只是取資料) 有路由才有連線 /拿它去連modules
 
-
-
 //取的所有商品+篩選
-router.get( '/',async(req,res)=>{
-    res.json([req.baseUrl,req.url]); //測試noded.js環境
+router.get("/", async (req, res) => {
+  res.json([req.baseUrl, req.url]); //測試noded.js環境
 }); //baseUrl=跟目錄   //req.url=目前目錄
 //後端通過路由來拿取前端的req資料，之後在res回去
 //接收前端來的req然後在在responce回去
 
-
-
 //新增商品測試 /home/add
+router.get("/add", async (req, res) => {
+  const p1 = new Home({
+    //new Home是實體化
+    oListName: "芒果",
+    oListPrice: "999",
+  });
+  const obj1 = await p1.save();
+  //await就像是帳號密碼,等待帳號密碼輸入才會執行後面
+  //!!!!await先執行再到async
 
-router.get('/add',async(req,res)=>{
-    const p1= new Home({  //new Home是實體化
-        iname='您好',
-        iprice='5959595',  
-    });
-    const obj1= await p1.save();
-    //await就像是帳號密碼,等待帳號密碼輸入才會執行後面
-    //!!!!await先執行再到async
-
-    res.json([obj1]);
+  res.json([obj1]);
 });
 
-
-
-
-
-module.exports=router
+module.exports = router;
 //單傳輸出這ㄍ
-
-
-
-
-
 
 //module 模組(home.js)
 //等號右手邊只向左邊
