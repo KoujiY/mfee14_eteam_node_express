0e3e91083e54356645890587189357a63b7a5a7d
wworveaory <wworveaory@gmail.com>
2021-09-18 14:31:59 +0800

    ㄊ
    ㄊ

--- modules/Home.js
+++ modules/Home.js
@@ -7,7 +7,6 @@ class Home {
       iId: 0, //商品編號
       iName: "", //商品名稱
       iImg: "[]", //商品圖片
-      iDiscr: "", //商品簡介
       iPrice: 0, //商品價格
     };
     this.data = { ...defaultData, ...data };
